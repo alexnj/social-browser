4229474ab25da7dd345c261c64c067ee0a5e0a78
Alex N. Jose <alexnj@gmail.com>
2014-07-09 11:01:00 -0700

    Updated test cases.

--- test/index.js
+++ test/index.js
@@ -4,13 +4,22 @@ var chai = require("chai"),
     detect = socialbrowser.detect;
 
 describe("#detect", function() {
-    it("detects facebook browser", function() {
+    it("detects facebook native browser", function() {
         var browser = detect({
             "headers": {
                 "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 [FBAN/FBIOS;FBAV/12.1.0.24.20;FBBV/3214247;FBDV/iPhone5,1;FBMD/iPhone;FBSN/iPhone OS;FBSV/7.1.2;FBSS/2; FBCR/AT&T;FBID/phone;FBLC/en_US;FBOP/5]"
             }
         });
 
-        expect(browser).to.have.property("major").and.is.equal.to("facebook");
+        expect(browser).to.have.property("major").and.is.equal("facebook");
+    });
+    it("detects twitter native browser", function() {
+        var browser = detect({
+            "headers": {
+                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 Twitter for iPhone"
+            }
+        });
+
+        expect(browser).to.have.property("major").and.is.equal("twitter");
     });
 });
